a549a4999fb23e5682501b497e22cc016a75597c
devg1120 <devg1120@gmail.com>
2026-05-28 09:21:33 +0000

    up

---
+++ case21_solid5/react-core_solid_diff/src/components/Emitter.tsx
@@ -0,0 +1,34 @@
+import { Context } from "../store";
+
+export const Emitter: FC = () => {
+    const { store } = useContext(Context);
+    const { choosing: pointing, selectingZone: zone, tableReactive } = store;
+    const table = tableReactive.current;
+
+    useEffect(() => {
+        if (table?.isInitialized && table.wire.onChange) {
+            table.wire.onChange({
+                table,
+                points: {
+                    pointing,
+                    selectingFrom: { y: zone.startY, x: zone.startX },
+                    selectingTo: { y: zone.endY, x: zone.endX },
+                },
+            });
+        }
+    }, [tableReactive]);
+
+    useEffect(() => {
+        if (table && table.wire.onSelect) {
+            table.wire.onSelect({
+                table,
+                points: {
+                    pointing,
+                    selectingFrom: { y: zone.startY, x: zone.startX },
+                    selectingTo: { y: zone.endY, x: zone.endX },
+                },
+            });
+        }
+    }, [pointing, zone]);
+    return null;
+};
