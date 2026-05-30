31d0639a54ababf64922e282ec3d10ec645d2463
devg1120 <devg1120@gmail.com>
2026-05-30 10:40:33 +0900

    up

--- case21_solid6/react-core/src/components/Cell.tsx
+++ case21_solid6/react-core/src/components/Cell.tsx
@@ -156,8 +156,8 @@ export const Cell: FC<Props> = ({
   const editingAnywhere = !!(table.wire.editingAddress || editingAddress);
 
   const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
-    console.log("handleDragStart", store().choosing, y,x)
-    //e.stopPropagation();
+    //console.log("handleDragStart", store().choosing, y,x)
+    e.stopPropagation();
     safePreventDefault(e);
 
     if (store().choosing.y == y && store().choosing.x == x) {
@@ -454,7 +454,7 @@ export const Cell: FC<Props> = ({
         // shift-key  abalable               TODO
         onMouseDown={handleDragStart} //NEW
         onTouchStart={handleDragStart} //NEW
-        onMouseEnter={handleDragging} //NEW   TODO
+        //onMouseEnter={handleDragging} //NEW   TODO
         onMouseUp={handleDragEnd} //NEW
       >
         <div class={"gs-cell-inner"} style={{}}>
